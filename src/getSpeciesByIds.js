59af9def0854b35874f596587e64eec43f0bc512
Tiago Alves <tiagoalvesfernandes81@gmail.com>
2022-09-03 19:56:52 -0300

    exercicio 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return ids.map((id) => species.filter((especie) => especie.id === id)).flat();
 }
+
+console.log(getSpeciesByIds('bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'));
 
 module.exports = getSpeciesByIds;
