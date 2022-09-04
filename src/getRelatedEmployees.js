437919b0473535b6c265953abd51216bee9a2dbe
Tiago Alves <tiagoalvesfernandes81@gmail.com>
2022-09-04 18:38:35 -0300

    exercicio 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,28 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
+
+const gerentesIds = ['9e7d4524-363c-416a-8759-8aa7e50c0992',
+  'fdb2543b-5662-46a7-badc-93d960fdc0a8', '0e7b460e-acf4-4e17-bcb3-ee472265db83'];
 
 function isManager(id) {
-  // seu código aqui
+  const resultado = gerentesIds.some((gerente) => gerente === id);
+  return resultado;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const arrayDeFuncionarios = [];
+    employees.forEach((funcionario) => {
+      funcionario.managers.forEach((gerentes) => {
+        if (gerentes === managerId) {
+          arrayDeFuncionarios.push(`${funcionario.firstName} ${funcionario.lastName}`);
+        }
+      });
+    });
+    return arrayDeFuncionarios;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
-
 module.exports = { isManager, getRelatedEmployees };
+
+// .some((elemento) => elemento.id === id);
