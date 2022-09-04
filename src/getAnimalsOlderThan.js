29ea16a44bb0b8904be307f2cb97f5488185bf69
Tiago Alves <tiagoalvesfernandes81@gmail.com>
2022-09-04 15:39:36 -0300

    exercicio 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+function getAnimalsOlderThan(animal, idade) {
+  // const teste = JSON.stringify(species);
+  // const testeJson = JSON.parse(teste);
+  const especie = species.find((nome) => nome.name === animal);
+  // especie.residents.forEach((element) => console.log(element.age));
+  const maisVelho = especie.residents.every((residentes) => residentes.age >= idade);
+  return maisVelho;
 }
 
+console.log(getAnimalsOlderThan('otters', 7));
 module.exports = getAnimalsOlderThan;
