9250c340275dd07ad95d1c45d8d438afd849235c
Tiago Alves <tiagoalvesfernandes81@gmail.com>
2022-09-04 16:36:44 -0300

    exercicio 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,9 +1,9 @@
-/* eslint-disable max-len */
 const data = require('../data/zoo_data');
 const { employees } = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  const funcionario = employees.find((nome) => nome.firstName === employeeName || nome.lastName === employeeName);
+  const funcionario = employees.find((nome) => nome.firstName === employeeName
+   || nome.lastName === employeeName);
   return funcionario === undefined ? {} : funcionario;
 }
 
