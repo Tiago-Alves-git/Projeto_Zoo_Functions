cd0cd24244e589911cc59fd70c81b8833219f05e
Tiago Alves <tiagoalvesfernandes81@gmail.com>
2022-09-05 21:56:55 -0300

    exercicio 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    const residentes = {};
+    data.species.forEach((specie) => {
+      residentes[specie.name] = specie.residents.length;
+    });
+    return residentes;
+  }
+  if (Object.keys(animal).length === 1) {
+    const findAnimal = data.species.find((qualAnimal) => (
+      qualAnimal.name === Object.values(animal)[0]
+    ));
+    return findAnimal.residents.length;
+  }
+  const findAnimalSexo = data.species.find((qualAnimal) => (
+    qualAnimal.name === Object.values(animal)[0]
+  ));
+  return findAnimalSexo.residents.filter((sexo) => sexo.sex === Object.values(animal)[1]).length;
 }
 
 module.exports = countAnimals;
