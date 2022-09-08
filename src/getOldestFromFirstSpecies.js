072fe6d960cfc11e37e8ae6ff39f6fef6e663338
Tiago Alves <tiagoalvesfernandes81@gmail.com>
2022-09-08 15:34:48 -0300

    exercicio 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const Animal = data.employees.find((pessoa) => pessoa.id === id).responsibleFor[0];
+  const animalMaisVelho = species.find((animal) => animal.id === Animal)
+    .residents.sort((a, b) => b.age - a.age)[0];
+  return Object.values(animalMaisVelho);
 }
 
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,27 @@
+const { species, hours } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (Object.values(species).some((animal) => animal.name === scheduleTarget) === true) {
+    return 'aa.b';
+  }
+  if (Object.keys(hours).some((dia) => dia === scheduleTarget) === true) {
+    return 'bb.a';
+  }
+  return 'cc.c';
+}
+function getDia(Parametro) {
+  return Parametro;
+}
+function getAnimal(Parametro) {
+  return Parametro;
+}
+function getTudo(Parametro) {
+  return Parametro;
 }
 
+console.log(getSchedule('Friday'));
+console.log(getDia);
+console.log(getAnimal);
+console.log(getTudo);
 module.exports = getSchedule;
