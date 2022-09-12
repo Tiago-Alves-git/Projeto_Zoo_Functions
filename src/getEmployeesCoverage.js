86d6eed0920cf47a454d4fd017c22cb7d44e237b
Tiago Alves <tiagoalvesfernandes81@gmail.com>
2022-09-12 02:04:09 -0300

    Exercicios 9

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,24 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function pegaPessoa(nameOrId) {
+  const Funcionario = Object.values(nameOrId).toLocaleString();
+  return employees.find((pessoa) => pessoa.firstName === Funcionario
+  || pessoa.lastName === Funcionario || pessoa.id === Funcionario);
+}
+function pegaAnimal(id) {
+  const Animais = [];
+  pegaPessoa(id).responsibleFor.forEach((AnimalID) => Animais.push(species
+    .find((Especie) => Especie.id === AnimalID)));
+  return Animais;
+}
+function getEmployeesCoverage(Parametro) {
+  return pegaAnimal(Parametro);
 }
 
+// console.log(getEmployeesCoverage(''));
+// console.log(getEmployeesCoverage({ name: 'Spry' }));
+// console.log(getEmployeesCoverage({ id: '4b40a139-d4dc-4f09-822d-ec25e819a5ad' }));
+// console.log(getEmployeesCoverage({ name: 'Sharonda' }));
+// console.log(getEmployeesCoverage({ name: 'Tiago' }));
 module.exports = getEmployeesCoverage;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,27 +1,51 @@
 const { species, hours } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const date = hours;
+
+const obj = {
+  Tuesday: { officeHour: `Open from ${date.Tuesday.open}am until ${date.Tuesday.close}pm`,
+  },
+  Wednesday: { officeHour: `Open from ${date.Wednesday.open}am until ${date.Wednesday.close}pm`,
+  },
+  Thursday: { officeHour: `Open from ${date.Thursday.open}am until ${date.Thursday.close}pm`,
+  },
+  Friday: { officeHour: `Open from ${date.Friday.open}am until ${date.Friday.close}pm`,
+  },
+  Saturday: { officeHour: `Open from ${date.Saturday.open}am until ${date.Saturday.close}pm`,
+  },
+  Sunday: { officeHour: `Open from ${date.Sunday.open}am until ${date.Sunday.close}pm`,
+  },
+  Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' },
+};
+
+function criaObjeto() {
+  obj.Tuesday.exhibition = (data.species.filter((item) => item.availability
+    .includes('Tuesday')).map((item) => item.name));
+  obj.Wednesday.exhibition = (data.species.filter((item) => item.availability
+    .includes('Wednesday')).map((item) => item.name));
+  obj.Thursday.exhibition = (data.species.filter((item) => item.availability
+    .includes('Thursday')).map((item) => item.name));
+  obj.Friday.exhibition = (data.species.filter((item) => item.availability
+    .includes('Friday')).map((item) => item.name));
+  obj.Saturday.exhibition = (data.species.filter((item) => item.availability
+    .includes('Saturday')).map((item) => item.name));
+  obj.Sunday.exhibition = (data.species.filter((item) => item.availability
+    .includes('Sunday')).map((item) => item.name));
+  return obj;
+}
 function getSchedule(scheduleTarget) {
-  if (Object.values(species).some((animal) => animal.name === scheduleTarget) === true) {
-    return 'aa.b';
+  const objetoCriado = criaObjeto();
+  const Animais = species.map((Animals) => Animals.name);
+  const Dias = Object.keys(obj);
+  if (Dias.includes(scheduleTarget)) {
+    return { [scheduleTarget]: objetoCriado[scheduleTarget] };
   }
-  if (Object.keys(hours).some((dia) => dia === scheduleTarget) === true) {
-    return 'bb.a';
+  if (Animais.includes(scheduleTarget)) {
+    return species.find((animal) => animal.name === scheduleTarget).availability;
   }
-  return 'cc.c';
-}
-function getDia(Parametro) {
-  return Parametro;
-}
-function getAnimal(Parametro) {
-  return Parametro;
-}
-function getTudo(Parametro) {
-  return Parametro;
+  return criaObjeto();
 }
 
-console.log(getSchedule('Friday'));
-console.log(getDia);
-console.log(getAnimal);
-console.log(getTudo);
+console.log(getSchedule('lions'));
 module.exports = getSchedule;
